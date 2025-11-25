64984f04c626b9fda08e99d0d9bf7cd07d4f24b9
Ivan Samusevich <samusevich.ivan@list.ru>
2025-11-25 19:14:14 +0300

    add banner

--- app/src/main/java/com/example/listshop/Authorization.java
+++ app/src/main/java/com/example/listshop/Authorization.java
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -14,15 +15,36 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.listshop.databinding.AutorizationBinding;
+import com.yandex.mobile.ads.banner.AdSize;
+import com.yandex.mobile.ads.banner.BannerAdView;
+import com.yandex.mobile.ads.common.AdRequest;
+import com.yandex.mobile.ads.common.AdRequestError;
+import com.yandex.mobile.ads.common.MobileAds;
+
 public class Authorization extends AppCompatActivity {
+
+    private AutorizationBinding binding;
     private EditText editUsername, editPassword;
     private ImageView imageYandex;
     private Button registration, enter;
+
+
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //todo Доделать рекламу
+        binding = AutorizationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.banner.setAdUnitId("R-M-17809850-2");
+        binding.banner.setAdSize(AdSize.BANNER_320x50);
+
+        final AdRequest adRequest = new AdRequest.Builder().build();
+
+        binding.banner.loadAd(adRequest);
 
         setContentView(R.layout.autorization);
 
@@ -73,6 +95,7 @@ public class Authorization extends AppCompatActivity {
             }
         });
     }
+
 
     private void openWebsite(){
         try {
